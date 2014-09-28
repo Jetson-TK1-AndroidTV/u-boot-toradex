9ab6348bac4bb5ea68c3986826849de534fac51f
Michael Scott <michael.scott@linaro.org>
2014-09-28 16:30:45 -0700

    board: nvidia: check bits 30/31 of pmc_scratch0 for reboot-recovery/bootloader
    if bit 31 is set at pmc_scratch0, the device was rebooted for recovery-mode
    in this case set the "recovery" environment to "1"
    
    if bit 30 is set at pmc_scratch0, the device was rebooted for bootloader-mode
    in this case we enter fastboot mode
    
    Signed-off-by: Michael Scott <michael.scott@linaro.org>
    
    Conflicts:
    	board/nvidia/common/board.c

--- board/nvidia/common/board.c
+++ board/nvidia/common/board.c
@@ -184,6 +184,7 @@ int board_late_init(void)
 	/* Make sure we finish initing the LCD */
 	tegra_lcd_check_next_stage(gd->fdt_blob, 1);
 #endif
+
 #if defined(CONFIG_TEGRA_SUPPORT_NON_SECURE)
 	if (tegra_cpu_is_non_secure()) {
 		printf("CPU is in NS mode\n");
@@ -192,6 +193,27 @@ int board_late_init(void)
 		setenv("cpu_ns_mode", "");
 	}
 #endif
+
+#ifdef CONFIG_TEGRA124
+	struct pmc_ctlr *pmc = (struct pmc_ctlr *)NV_PA_PMC_BASE;
+	unsigned int scratch = readl(&pmc->pmc_scratch0);
+
+	writel(scratch & ~(1 << 30) & ~(1 << 31), &pmc->pmc_scratch0);
+	if (scratch & (1 << 30)) {
+		printf("Found reboot-mode = bootloader!\n");
+		setenv("recovery", "0");
+		do_fastboot(NULL, 0, 0, NULL);
+	}
+	else if (scratch & (1 << 31)) {
+		printf("Found reboot-mode = recovery!\n");
+		setenv("recovery", "1");
+	}
+	else {
+		printf("No reboot-mode found\n");
+		setenv("recovery", "0");
+	}
+#endif
+
 	return 0;
 }
 
